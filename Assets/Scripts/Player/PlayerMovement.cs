019829d4f4b282b81731cc8fcc4e8ff118cf6e0e
Patrik Durasek <patrik.durasek@gmail.com>
2018-07-09 23:07:13 +0200

    Add initial project skeleton | First city WIP
    - Add 2D project skeleton
    - Add WIP map of the first city
    - Add Tile and Character spritesheets

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Direction currentDirection;
+    private Vector2 playerInput;
+    private bool isMoving = false;
+    private Vector3 startPosition;
+    private Vector3 endPosition;
+    private float time;
+    private int moveDistance = 4;
+
+    public float moveSpeed = 0.2f;
+    public Sprite northSprite;
+    public Sprite eastSprite;
+    public Sprite southSprite;
+    public Sprite westSprite;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int x = 0;
+        int y = 0;
+        Vector2 target = Vector2.zero;
+        
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            x = -moveDistance;
+            y = 0;
+            currentDirection = Direction.West;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            x = moveDistance;
+            y = 0;
+            currentDirection = Direction.East;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            x = 0;
+            y = -moveDistance;
+            currentDirection = Direction.South;
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            x = 0;
+            y = moveDistance;
+            currentDirection = Direction.North;
+        }
+        
+        target = new Vector2(x, y);
+
+        switch (currentDirection)
+        {
+            case Direction.North:
+                gameObject.GetComponent<SpriteRenderer>().sprite = northSprite;
+                break;
+            case Direction.South:
+                gameObject.GetComponent<SpriteRenderer>().sprite = southSprite;
+                break;
+            case Direction.East:
+                gameObject.GetComponent<SpriteRenderer>().sprite = eastSprite;
+                break;
+            case Direction.West:
+                gameObject.GetComponent<SpriteRenderer>().sprite = westSprite;
+                break;
+        }
+
+        transform.Translate(target * moveSpeed * Time.deltaTime);
+        Debug.Log(transform.position.x + " + " + x);
+        Debug.Log(transform.position.y + " + " + y);
+    }
+}
+
+enum Direction
+{
+    North,
+    East,
+    South,
+    West
+}
